f37091bb57f75011de7e554a189766273984a4a6
AbelChiriboga <abelsantiago3110@gmail.com>
2026-04-24 22:19:05 +0000

    Se agrega informacion personal en la pagina web

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("Welcome to Abel Chiriboga's portfolio 🚀");
+
+// pequeño efecto
+document.addEventListener("DOMContentLoaded", () => {
+    document.body.style.opacity = 0;
+    setTimeout(() => {
+        document.body.style.transition = "opacity 1s";
+        document.body.style.opacity = 1;
+    }, 100);
+});
